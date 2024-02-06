40709aabcc09b390c696b28ed9b9312bc2b506eb
unknown <evallardy@gmail.com>
2024-02-06 16:48:15 -0600

    primera prueba 40

--- whatsapp/api.py
+++ whatsapp/api.py
@@ -86,7 +86,7 @@ def mensaje(request):
     bitacora = Bitacora(descripcion = 'Termina Json')
     bitacora.save()
 
-    bitacora = Bitacora(descripcion = request.GET.get('hub', 'Sin dato'))
+    bitacora = Bitacora(descripcion = request)
     bitacora.save()
 
 
