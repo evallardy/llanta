145d795df41e5f560aeb15879ab3368bf1cef0be
unknown <evallardy@gmail.com>
2024-02-06 17:01:06 -0600

    primera prueba 50

--- whatsapp/api.py
+++ whatsapp/api.py
@@ -89,7 +89,7 @@ def mensaje(request):
     # bitacora = Bitacora(descripcion = request.data)
     # bitacora.save()
 
-    bitacora = Bitacora(descripcion = datos['messages'])
+    bitacora = Bitacora(descripcion = datos['entry'][0])
     bitacora.save()
 
 
