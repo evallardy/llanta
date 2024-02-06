7d9c684b1f4021ba0a5d1727860b0880b1be8196
unknown <evallardy@gmail.com>
2024-02-06 03:57:49 -0600

    primera prueba 18

--- whatsapp/api.py
+++ whatsapp/api.py
@@ -61,7 +61,7 @@ def registra_asesor(comunicacion):
 @api_view(['POST','GET'])
 def mensaje(request):
     # Guardado de la información que llega tanto el meodo como la información
-    bitacora = Bitacora(descripcion = json.dumps(request))
+    bitacora = Bitacora(descripcion = request)
     bitacora.save()
 
     datos = request.data
